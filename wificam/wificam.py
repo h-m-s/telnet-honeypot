8384be9a79c4e836a7f95582b4120c57872a8a5a
wintermanc3r <timothy.britton@holbertonschool.com>
2017-03-19 21:08:19 +0000

    Now perfectly matches a HEAD response from a GoAhead camera! Should be Shodan-ready.

--- wificam/wificam.py
+++ wificam/wificam.py
@@ -1,46 +1,60 @@
-
-#!/usr/bin/env python
+#!/usr/bin/python3
 """
-attempt to emulate the following article:
+Research tool based on the following article:
 https://pierrekim.github.io/blog/2017-03-08-camera-goahead-0day.html
 
-headers set-up to appear as a GoAhead cam on Shodan:
+Spins a really dead simple webserver that gives the proper GET and HEAD
+responses for a GoAhead camera.
+
+When the script from the article is ran, the server will give a response
+with a planted username/pass appearing in the correct spot for the script
+to grab it, and think it properly injected the ftp server with
+the netcat listener.
+
+This script alone does NOT currently start a netcat instance with the
+attacker's server, but does grab the address of the server they're using
+and the port they expect to see a netcat connection.
+
+Next step is to implement this by starting a telnet client with a
+HoneyTelnetServer with a special username/pass that puts the
+HoneyTelnetServer in 'netcat mode', which should be basically
+the same exact thing, without a prompt. May have to figure out
+if we need to strip out any telnet control characters, etc? But
+we can then connect this client directly to the netcat listener,
+so for all intents and purposes, it should look exactly like the script
+succeeded.
+
+headers are set-up to appear as a GoAhead cam on Shodan:
 https://www.shodan.io/search?query=GoAhead+5ccc069c403ebaf9f0171e9517f40e41
-
-TODO:
-***
-connect to the netcat listener! we already parse out
-where we're supposed to connect to..
-
-i think we could essentially write a telnet client,
-connect to the honey server, and pipe it all through netcat
-so it looks like it's a direct line to /bin/sh
-
-just open the client, give a special user pass that
-starts the server in 'netcat mode' where
-there's no prompt, and pass control to netcat :)
-
-server could look for user: net password: cat
-and set a flag on the client obj that
-determines if we're in telnet or netcat mode.
-***
-
-***
-change headers so we appear the same as a GoAhead cam on shodan !!!!
-***
 
 """
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
-global count
 count = 0
 
-class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+server_address = ('0', 81)
+"""
+Set the address/port for your server here.
+'0' will bind to all addresses. Port should be changed
+here, as it will effect the header. Probably doesn't
+matter if the header doesn't match the port, but
+we're going for authenticity. ;)
+"""
+
+class GoAheadHandler(BaseHTTPRequestHandler):
+  """
+  Server to mimic a GoAhead camera web server, and appear on Shodan
+  as a possible vulnerable camera, that returns the proper responses
+  to the '0day' script to make attackers think they actually grabbed
+  credentials or created a backdoor.
+  """
   def date_time_string(self, timestamp=None):
-    """ Let's override the date_time_string function from the base
+    """
+    Let's override the date_time_string function from the base
     so we look identical to a GoAhead cam on Shodan. Nitpicky detail,
-    but let's not confuse any bots!"""
+    but let's not confuse any bots!
+    """
     if timestamp is None:
       timestamp = time.time()
       year, month, day, hh, mm, ss, wd, y, z = time.gmtime(timestamp)
@@ -52,26 +66,61 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
 
   def do_HEAD(self):
+    """
+    Just causes a HEAD request call up the default do_GET message.
+    """
     self.do_GET()
 
   def do_GET(self):
+    """
+    Handles all GET requests, including the attacks.
+    """
+    global count
     self.server_version = "GoAhead-Webs"
     self.sys_version = ""
-    global count
     msg = "Hello world!"
     print("Got a GET request.")
     if "/system.ini?loginuse&loginpas" in self.requestline:
+      """
+      This is the line the script uses to snag credentials.
+      Currently this just fills in the execess space with 'b's.
+      It'd be pretty obvious if you were dumping the output of the script,
+      but if you just compile the 0-day script as given and
+      run it against this server, it prints to the screen:
+
+      [+] bypassing auth ... done
+          login = minad
+          pass  = damin
+      [+] planting payload ... done
+      [+] executing payload ... done
+      [+] cleaning payload ... done
+      [+] cleaning payload ... done
+      [+] enjoy your root shell on REMOTE_HOST:REMOTE_PORT
+
+      It only checks to see that it can harvest the login/pass from the
+      step that grabs credentials, and never checks the response from
+      the payload itself.
+      """
       print("Attacker attempting to get credentials! Sending fake creds...")
       count = 1
-      msg = ("a" * 137) #filler, attack script will skip this to look for uname
-      msg += "admin" #username
+      msg = ("b" * 137) #filler, attack script will skip this to look for uname
+      msg += "minad" #username
       msg += ("\0" * 27) # more filler, script skips forward 27 for pass
-      msg += "admin" #password
+      msg += "damin" #password
       self.send_response(200)
       self.send_header('Content-type','text/html')
       self.end_headers()
       self.wfile.write(bytes.fromhex('0a0a0a0a01') + bytes(msg, "utf8"))
     elif count > 0:
+      """
+      The line that follows the payload to get the server credentials
+      contains the login/pass (which should match the ones you provided
+      above), but it also contains the server and port the attacker should
+      have a netcat listener open on.
+
+      You'd need to connect to this port to make the attacker think they have
+      full access.
+      """
       for line in self.requestline.split("&"):
         if "loginuse" in line:
           print("Attacker using login: {}".format(line.split("=")[1]))
@@ -82,11 +131,23 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
           print("Attacker remote server: {} port: {}".format(temp[0], temp[1]))
       count -= 1
     else:
+      """
+      This basically just covers any old GET or HEAD request.
+      Fills in the proper headers for a GoAhead camera and the default 401.
+      Would be nice to actually have some sort of landing page for the correct
+      user/password, because it's going to make the attacker think they obtained
+      credentials.
+
+      Perhaps just log every username/pass entered, and set the credentials
+      harvested from the script to something unique, so we'd know who
+      successfully launched the script, and never have anything past this
+      portal.
+      """
       msg = "<html><head><title>Document Error: Unauthorized</title></head>\n"
       msg += "                <body><h2>Access Error: Unauthorized</h2>\n"
       msg += "                <p>Access to this document requires a User ID</p></body></html>\n\n"
       self.send_response(401)
-      self.send_header('WWW-Authenticate', 'Digest realm="GoAhead", domain=":81",qop="auth", nonce="a32f2b51bcb55c24d003a21be5ec0345", opaque="5ccc069c403ebaf9f0171e9517f40e41",algorithm="MD5", stale="FALSE"')
+      self.send_header('WWW-Authenticate', 'Digest realm="GoAhead", domain=":{}",qop="auth", nonce="a32f2b51bcb55c24d003a21be5ec0345", opaque="5ccc069c403ebaf9f0171e9517f40e41",algorithm="MD5", stale="FALSE"'.format(server_address[1]))
       self.send_header('Pragma', 'no-cache')
       self.send_header('Cache-Control', 'no-cache')
       self.send_header('Content-type','text/html')
@@ -95,11 +156,12 @@ class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     return
 
 def run():
-  global count
-  count = 0
+  """
+  Loop to start the server up, bound to all addresses, on the given port.
+  If you change the port, it should change the headers to reflect your new port.
+  """
   print('starting server...')
-  server_address = ('0', 81)
-  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  httpd = HTTPServer(server_address, GoAheadHandler)
   print('running server...')
   httpd.serve_forever()
 
