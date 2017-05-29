64ebcba586d2267727910a89a9dd2134d98822f3
wintermanc3r <timothy.britton@holbertonschool.com>
2017-05-29 02:05:39 +0000

    Pushing changes to postgres/flask integration branch:

--- postgres_db/client.py
+++ postgres_db/client.py
@@ -60,6 +60,8 @@ def sanitize_pattern(client):
     return(input_string, md5)
 
 def process_attack(client):
+    if client.ip == '127.0.0.1':
+        return
     try:
         sanitized_pattern, md5 = sanitize_pattern(client)
         add_attack(client.ip, sanitized_pattern, md5)
