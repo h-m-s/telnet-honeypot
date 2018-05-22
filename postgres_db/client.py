90932de8ad96f95592e0eebdd531672e672b5fd0
wintermanc3r <timothy.britton@holbertonschool.com>
2018-05-22 07:40:41 +0000

    Wasn't catching this exception anymore

--- postgres_db/client.py
+++ postgres_db/client.py
@@ -22,7 +22,7 @@ def sanitize_pattern(client):
                 r"(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?:?[0-9]*)",
                 "1.1.1.1", line)
         client_list += [line]
-    input_string = '\n'.join(client_list)
+    input_string = ';'.join(client_list)
     md5 = hashlib.md5(input_string.encode("utf8")).hexdigest()
     return(input_string, md5)
 
@@ -30,7 +30,11 @@ def process_attack(client):
     logger = logging.getLogger('telnet')
     if client.ip == '127.0.0.1':
         return
-    sanitized_pattern, md5 = sanitize_pattern(client)
+    try:
+        sanitized_pattern, md5 = sanitize_pattern(client)
+    except TypeError:
+        """ If sanitize_pattern returns None, there wasn't enough input to form a pattern. """
+        return
     logger.info("Attack pattern stored", extra={
                                              'client_ip': client.ip,
                                              'client_port': client.remote_port,
